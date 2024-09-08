e4414f54747e358bc441a50d05391642f7332019
Snshadow <randy0007@naver.com>
2024-09-08 20:49:12 +0900

    feat: RawReadWriter(wip)

--- w32api/types.go
+++ w32api/types.go
@@ -1,7 +1,38 @@
 package w32api
 
 import (
+	"unsafe"
+
 	"golang.org/x/sys/windows"
+)
+
+// application-defined callback functions for ReadEncryptedFileRaw and WriteEncryptedFileRaw
+
+/*
+DWORD PfeExportFunc(
+  [in]           PBYTE pbData,
+  [in, optional] PVOID pvCallbackContext,
+  [in]           ULONG ulLength
+)
+{...}
+*/
+type PRE_EXPORT_FUNC func(data *byte, callbackContext unsafe.Pointer, length *uint32) uintptr
+
+/*
+DWORD PfeImportFunc(
+  [in]           PBYTE pbData,
+  [in, optional] PVOID pvCallbackContext,
+  [in, out]      PULONG ulLength
+)
+{...}
+*/
+type PRE_IMPORT_FUNC func(data *byte, callbackContext unsafe.Pointer, length uint32) uintptr
+
+// flag for OpenEncryptedFileRaw
+const (
+	CREATE_FOR_IMPORT = 1
+	CREATE_FOR_DIR    = 2
+	OVERWRITE_HIDDEN  = 4
 )
 
 type EFS_CERTIFICATE_BLOB struct {
